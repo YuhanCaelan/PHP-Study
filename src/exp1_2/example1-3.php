a1a16cda0aa2d6d1068d0208c647250828417a4a
Lin Yuhan <heshibi985@gmail.com>
2026-03-13 11:49:51 +0800

    Initial commit

---
+++ src/exp1_2/example1-3.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>基本标记</title>
+    </head>
+    <body>
+        <h1 align="center">标题标记</h1>
+        <p align="center">分段标记</p>
+        <b>换行标记</b><br>
+        <center> 对中标记</center><br>
+        <hr size="2" width="300"noshade><br>
+        <div align="center"><font size="5" color="red" face="黑体">块标记</></div>
+        <a href="http://www.163.com">超链接</a><br>
+    </body>
+</html>
---
+++ src/exp1_2/example1-4.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>基本标记</title>
+    </head>
+    <body bgcolor="blue">
+        <h1 align="center"><marquee behavior="scroll" direction="left">标题标记</marquee></h1>
+        <p align="center">分段标记</p>
+        <p align="center"><img src="https://picsum.photos/400/250" width="400" height="250" alt="示例图片"></p>
+        <b>换行标记</b><br>
+        <center> 对中标记</center><br>
+        <hr size="2" width="300"noshade><br>
+        <div align="center"><font size="5" color="red" face="黑体">块标记</font></div>
+        <a href="http://www.163.com">超链接</a><br>
+    </body>
+</html>
